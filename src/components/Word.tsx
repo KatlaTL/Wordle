302991ffe3b2d4a4daeca44a003b7fb193abf22b
Asger <regsa13@gmail.com>
2026-02-12 15:19:18 +0100

    first commit

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -1,39 +1,15 @@
-import { useEffect } from "react";
 import Letter from "./Letter";
 
 type WordProps = {
     word: string;
-    setWord: (newWord: string) => void;
 }
 
-const Word = ({ word, setWord }: WordProps) => {
-    console.log(word)
+const Word = ({ word }: WordProps) => {
 
     const letters = Array.from({ length: 5 }, (_, i) => {
         const letter = word[i];
         return <Letter key={i} letter={letter} />
     });
-
-    const addletter = (letter: string) => {
-        console.log(word)
-        if (word.length < 5) {
-            setWord(word + letter);
-        }
-    }
-
-    useEffect(() => {
-        const handleKeyDown = (event: KeyboardEvent) => {
-            const key = event.key;
-
-            // Only allow letters a-z or A-Z
-            if (/^[a-zA-Z]$/.test(key)) {
-                addletter(key);
-            }
-        }
-        window.addEventListener("keydown", handleKeyDown);
-
-        return () => window.removeEventListener("keydown", handleKeyDown);
-    }, [])
 
     return (
         <div className="flex gap-3" >
--- src/pages/game/GameBoard.tsx
+++ src/pages/game/GameBoard.tsx
@@ -1,17 +1,51 @@
 import Word from "../../components/Word";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const GameBoard = () => {
     const [words, setWords] = useState<string[]>(Array(6).fill(""));
+    const [activeRow, setActiveRow] = useState<number>(0);
 
-    const updateWord = (index: number, newWord: string) => {
-        setWords(prev => {
-            const updated = [...prev];
-            updated[index] = newWord;
-            return updated;
-        })
-    }
+    /**
+     * Listen on keydown events from the keyboard 
+     */
+    useEffect(() => {
+        const handleKeyDown = (event: KeyboardEvent) => {
+            const key = event.key;
+
+            setWords(prev => {
+                const updated = [...prev];
+                const currentWord = updated[activeRow];
+
+                // Only allow letters a-z or A-Z
+                if (/^[a-zA-Z]$/.test(key)) {
+
+                    if (currentWord.length < 5) {
+                        updated[activeRow] = currentWord + key;
+                    }
+
+                    return updated;
+                }
+
+                // Backspace
+                if (key === "Backspace") {
+                    updated[activeRow] = currentWord.slice(0, -1);
+                }
+
+                // Enter → move to next row
+                if (key === "Enter" && currentWord.length === 5) {
+                    setActiveRow(prev => prev + 1);
+                }
+
+                return updated;
+            })
+
+
+        }
+        window.addEventListener("keydown", handleKeyDown);
+
+        return () => window.removeEventListener("keydown", handleKeyDown);
+    }, [activeRow])
 
     return (
         <div className="flex justify-center my-10">
@@ -20,7 +54,6 @@ const GameBoard = () => {
                     <Word
                         key={i}
                         word={word}
-                        setWord={(newWord: string) => updateWord(i, newWord)}
                     />
                 ))}
 
